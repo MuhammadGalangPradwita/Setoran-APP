80586823209d3676ed28d488a49ad1f1f0b5649f
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2024-12-11 15:10:51 +0700

    add camera functionality

--- tubes_mobpro/lib/main.dart
+++ tubes_mobpro/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tubes_mobpro/tubes/camera_service.dart';
 import 'package:tubes_mobpro/tubes/pages/get_started_page.dart';
 import 'package:tubes_mobpro/notification_service.dart';
 
@@ -9,14 +10,40 @@ import 'package:tubes_mobpro/notification_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService().init();
+  await CameraService.instance.initializeCameras();
   // debugPaintSizeEnabled = false;
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    requestStoragePermission();
+    requestCameraStatus();
+  }
+
+  void requestCameraStatus() async {
+    var cameraStatus = await Permission.camera.status;
+    if (!cameraStatus.isGranted) {
+      await Permission.camera.request();
+    }
+  }
+
+  void requestStoragePermission() async {
+    var photoStatus = await Permission.photos.status;
+    if (!photoStatus.isGranted) {
+      await Permission.photos.request();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
---
+++ tubes_mobpro/lib/tubes/pages/edit_id_data_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:tubes_mobpro/tubes/themes/app_theme.dart';
+import 'package:tubes_mobpro/tubes/widgets/button_widgets.dart';
+import 'package:tubes_mobpro/tubes/widgets/textField_widget.dart';
+
+class EditIDDataPage extends StatefulWidget {
+  const EditIDDataPage({super.key});
+
+  @override
+  State<EditIDDataPage> createState() => _EditIDDataPageState();
+}
+
+class _EditIDDataPageState extends State<EditIDDataPage> {
+  TextEditingController _type = TextEditingController();
+  TextEditingController _no = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _type.text = "KTP";
+    _no.text = "1234567894561238";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.N0,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: const Text("Edit ID"),
+        backgroundColor: AppColors.B400,
+        foregroundColor: AppColors.N0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextfieldWidget(
+              readOnly: true,
+              label: "Type",
+              controller: _type,
+            ),
+            const Gap(16),
+            TextfieldWidget(
+              readOnly: true,
+              label: "ID No",
+              controller: _no,
+            ),
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: ButtonWidget.primary(
+                            label: "Continue", press: () {})),
+                  ],
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
