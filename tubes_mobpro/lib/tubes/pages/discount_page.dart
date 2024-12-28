885978afc71e189d033198a1cb5438a7df7348c8
haidar <haidarah134@gmail.com>
2024-12-28 07:21:23 +0700

    connect with voucher api

--- tubes_mobpro/lib/tubes/pages/discount_page.dart
+++ tubes_mobpro/lib/tubes/pages/discount_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:tubes_mobpro/tubes/api_utilities/voucher.dart';
+import 'package:tubes_mobpro/tubes/models/voucher.dart';
 import 'package:tubes_mobpro/tubes/themes/app_theme.dart';
 
 List<Map<String, dynamic>> discounts = [
@@ -23,10 +26,37 @@ List<Map<String, dynamic>> discounts = [
   }
 ];
 
-class DiscountPage extends StatelessWidget {
-  DiscountPage({super.key});
+class DiscountPage extends StatefulWidget {
+  @override
+  State<DiscountPage> createState() {
+    return _DiscountPageState();
+  }
+}
+
+class _DiscountPageState extends State<DiscountPage> {
 
   final PanelController slidingPanelController = PanelController();
+
+  bool loading = true;
+  List<Voucher> vouchers = [];
+
+  void refreshVoucher() {
+    setState(() {
+      vouchers = [];
+      loading = true;
+    });
+    VoucherAPi.getActive().then((res) {
+      setState(() {
+        vouchers = res;
+        loading = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    refreshVoucher();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -157,98 +187,121 @@ class DiscountPage extends StatelessWidget {
         ),
         minHeight: 0,
         body: SafeArea(
-          child: Container(
-            color: const Color.fromARGB(255, 235, 235, 235),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Voucher",
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        color: AppColors.N800,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Card(
-                    elevation: 0,
-                    color: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+          child: SingleChildScrollView(
+            child: Container(
+              color: const Color.fromARGB(255, 235, 235, 235),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Voucher",
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.poppins(
+                          fontSize: 12,
+                          color: AppColors.N800,
+                          fontWeight: FontWeight.w600),
                     ),
-                    child: IntrinsicHeight(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            flex:2,
-                            child: Container(
-                              // color: Colors.white,
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  bottomLeft: Radius.circular(10)
-                                  )
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(7),
-                                child: Column(
-                                  children: [
-                                      Text('New Member',style: GoogleFonts.poppins(
-                                        fontSize: 10,
-                                        color: Colors.blue,
-                                        fontWeight: FontWeight.w600
+                    Card(
+                      elevation: 0,
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IntrinsicHeight(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              flex:2,
+                              child: Container(
+                                // color: Colors.white,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10)
+                                    )
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(7),
+                                  child: Column(
+                                    children: [
+                                        Text('New Member',style: GoogleFonts.poppins(
+                                          fontSize: 10,
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.w600
+                                          ),
                                         ),
-                                      ),
-                                      Text('SALE',style: GoogleFonts.poppins(
-                                        fontSize: 16,
-                                        color: Colors.blue,
-                                        fontWeight: FontWeight.w600),
-                                      ),
-                                      const SizedBox(height:57, width: 75,child:  Image(image: AssetImage('assets/images/voucher-discount.png')))
-                                  ],
+                                        Text('SALE',style: GoogleFonts.poppins(
+                                          fontSize: 16,
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.w600),
+                                        ),
+                                        const SizedBox(height:57, width: 75,child:  Image(image: AssetImage('assets/images/voucher-discount.png')))
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            flex: 3,
-                            child: Stack(
-                              children: [
-                                  SizedBox(  
-                                  width: 189,
-                                  height: 115,
-                                ),
-                                const Positioned(
-                                  top: -10,
-                                  right: 0,
-                                  child: SizedBox(
+                            Expanded(
+                              flex: 3,
+                              child: Stack(
+                                children: [
+                                    SizedBox(  
                                     width: 189,
                                     height: 115,
-                                    child: Image(image: AssetImage('assets/images/voucher.png'))),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
+                                  ),
+                                  const Positioned(
+                                    top: -10,
+                                    right: 0,
+                                    child: SizedBox(
+                                      width: 189,
+                                      height: 115,
+                                      child: Image(image: AssetImage('assets/images/voucher.png'))),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  const Gap(10),
-                  Text(
-                    "Discount Code",
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        color: AppColors.N800,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  const Gap(10),
-                  ...discountsCards
-                ],
+                    const Gap(10),
+                    Row(
+                      children: [
+                        Text(
+                          "Discount Code",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              color: AppColors.N800,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Spacer(),
+                        InkWell(
+                          child: Icon(Icons.refresh),
+                          onTap: () => refreshVoucher(),
+                        )
+                      ],
+                    ),
+                    const Gap(10),
+                    ...vouchers.map((item) => Column(
+                          children: [
+                            DiscountCard(
+                              title: "Diskon ${item.persen}%",
+                              code: item.kodeVoucher,
+                              description: item.namaVoucher,
+                              endDate: item.tanggalAkhir,
+                              slidingPanelController: slidingPanelController,
+                            )
+                          ],
+                    )).toList(),
+                    loading ? Center(child: CircularProgressIndicator()) : Gap(0),
+                    Gap((150 * (4 - vouchers.length)).toDouble()),
+                  ],
+                ),
               ),
             ),
           )
@@ -281,11 +334,12 @@ class DiscountPage extends StatelessWidget {
 }
 
 class DiscountCard extends Card {
-  const DiscountCard({super.key, this.title='Diskon', this.code='SALECODE', this.description='Valid', this.slidingPanelController});
+  const DiscountCard({super.key, this.title='Diskon', this.code='SALECODE', this.description='Valid', this.endDate="12-12-9999", this.slidingPanelController});
 
   final String title;
   final String code;
   final String description;
+  final String endDate;
   final PanelController? slidingPanelController;
 
   @override
@@ -348,6 +402,18 @@ class DiscountCard extends Card {
                               // Spacer(),
                               const Spacer(), // gak tau biar container width fill parent gimana
                               InkWell(
+                                onTap: () {
+                                  Clipboard.setData(ClipboardData(text: code));
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Code saved to clipboard"),
+                                      action: SnackBarAction(
+                                        label: "close", 
+                                        onPressed: () => 
+                                          ScaffoldMessenger.of(context).clearSnackBars()
+                                        ),
+                                      ));
+                                },
                                 child: Text(
                                   'Copy',
                                   textAlign: TextAlign.left,
@@ -362,13 +428,26 @@ class DiscountCard extends Card {
                         ),
                       ),
                       const Gap(8),
-                      Text(
-                        description,
-                        textAlign: TextAlign.left,
-                        style: GoogleFonts.poppins(
-                            fontSize: 10,
-                            color: AppColors.N800,
-                            fontWeight: FontWeight.w400),
+                      Row(
+                        children: [
+                          Text(
+                            description,
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.poppins(
+                                fontSize: 10,
+                                color: AppColors.N800,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          Spacer(),
+                          Text(
+                            "Valid until: ${endDate}",
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.poppins(
+                                fontSize: 10,
+                                color: AppColors.N800,
+                                fontWeight: FontWeight.w400),
+                          )
+                        ],
                       ),
                       const Gap(8),
                       InkWell(
