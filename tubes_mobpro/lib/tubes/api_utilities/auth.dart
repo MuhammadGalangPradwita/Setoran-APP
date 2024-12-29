9ff6614a6789f0fd1901a35b7b556670550742d0
haidar <haidarah134@gmail.com>
2024-12-29 21:57:34 +0700

    catch auth error

--- tubes_mobpro/lib/tubes/api_utilities/auth.dart
+++ tubes_mobpro/lib/tubes/api_utilities/auth.dart
@@ -7,45 +7,53 @@ import 'package:http/http.dart' as http;
 // belum ada register sebagai apa nya (pelanggan/mitra)
 class AuthApi extends BaseApi {
   static Future<bool> login(String email, String password) async {
-    var response = await http.post(Uri.parse(BaseApi.backendHost + "/api/login"), 
-      body:{
-        "email": email,
-        "password": password
-      },
-      headers: {
-      'Accept': 'application/json',
+    try {
+        var response = await http.post(Uri.parse(BaseApi.backendHost + "/api/login"), 
+        body:{
+          "email": email,
+          "password": password
+        },
+        headers: {
+        'Accept': 'application/json',
+        }
+      );
+
+      // kadang ada return message/error dari backend, return type nya bisa di ubah kalau mau pake
+      if (response.ok) {
+        final data = jsonDecode(response.body);
+
+        BaseApi.storage.write(key: "access_token", value: data["access_token"]);
+        return true;
       }
-    );
+    } catch (e) {
 
-    // kadang ada return message/error dari backend, return type nya bisa di ubah kalau mau pake
-    if (response.ok) {
-      final data = jsonDecode(response.body);
-
-      BaseApi.storage.write(key: "access_token", value: data["access_token"]);
-      return true;
     }
+
 
     return false;
   }
 
   static Future<bool> register(String username, String email, String password) async {
-    var response = await http.post(Uri.parse(BaseApi.backendHost + "/api/register"), 
-      body:{
-        "nama": username,
-        "email": email,
-        "password": password
-      },
-      headers: {
-      'Accept': 'application/json',
-      }
-    );
+    try {
+        var response = await http.post(Uri.parse(BaseApi.backendHost + "/api/register"), 
+        body:{
+          "nama": username,
+          "email": email,
+          "password": password
+        },
+        headers: {
+        'Accept': 'application/json',
+        }
+      );
 
-    // kadang ada return message/error dari backend, return type nya bisa di ubah kalau mau pake
-    if (response.ok) {
-      return true;
+      // kadang ada return message/error dari backend, return type nya bisa di ubah kalau mau pake
+      if (response.ok) {
+        return true;
+      }
+    } catch (e) {
+
     }
     
-    print(response.body);
     return false;
   }
 
