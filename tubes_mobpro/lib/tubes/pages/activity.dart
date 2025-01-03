447498b873f5fea8c12253475f59a9d31ea889ab
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2025-01-04 03:20:40 +0700

    Refactor activity page and detail activity page

--- tubes_mobpro/lib/tubes/pages/activity.dart
+++ tubes_mobpro/lib/tubes/pages/activity.dart
@@ -3,6 +3,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:tubes_mobpro/tubes/api_utilities/pelanggan.dart';
+import 'package:tubes_mobpro/tubes/api_utilities/transaksi.dart';
+import 'package:tubes_mobpro/tubes/models/pelanggan.dart';
+import 'package:tubes_mobpro/tubes/models/transaksi.dart';
+import 'package:tubes_mobpro/tubes/pages/auth_check.dart';
 import 'package:tubes_mobpro/tubes/pages/detail_activity_page.dart';
 import 'package:tubes_mobpro/tubes/themes/app_theme.dart';
 import 'package:tubes_mobpro/tubes/widgets/transaction_card.dart';
@@ -63,27 +69,30 @@ enum TimeFilter { today, week, month }
 
 class _ActivityPageState extends State<ActivityPage> {
   Map<String, dynamic> _items = {};
+  late Future<List<Transaksi>?> dataList;
 
   TimeFilter selectedPeriod = TimeFilter.week;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      readJSON();
-    });
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+    //   // readJSON();
+    //   fetchData();
+    // });
+    dataList = fetchData();
   }
 
   TabBar get _tabBar => TabBar(
         tabs: const [
           Tab(
-            text: "Today",
+            text: "Last Week",
           ),
           Tab(
-            text: "This Week",
+            text: "Last Month",
           ),
           Tab(
-            text: "This Month",
+            text: "Earlier",
           ),
         ],
         dividerColor: Colors.transparent,
@@ -99,28 +108,48 @@ class _ActivityPageState extends State<ActivityPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-          backgroundColor: AppColors.N0,
-          appBar: AppBar(
-            // leading: IconButton(
-            //     onPressed: () {}, icon: const Icon(Icons.arrow_back)),
-            title: const Text("Activity"),
-            backgroundColor: AppColors.B400,
-            foregroundColor: AppColors.N0,
-            bottom: PreferredSize(
-                preferredSize: _tabBar.preferredSize,
-                child: Material(
-                  color: AppColors.N0,
-                  child: _tabBar,
-                )),
-          ),
-          body: TabBarView(children: [
-            todayTab(),
-            weekTab(),
-            weekTab(),
-          ])),
-    );
+        length: 3,
+        child: Scaffold(
+            backgroundColor: AppColors.N0,
+            appBar: AppBar(
+              title: const Text("Activity"),
+              backgroundColor: AppColors.B400,
+              foregroundColor: AppColors.N0,
+              bottom: PreferredSize(
+                  preferredSize: _tabBar.preferredSize,
+                  child: Material(
+                    color: AppColors.N0,
+                    child: _tabBar,
+                  )),
+            ),
+            body: FutureBuilder(
+              future: dataList,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    return _buildContent(snapshot.data as List<Transaksi>);
+                  }
+                }
+                return const Center(
+                  child: Text("No Data"),
+                );
+              },
+            )));
+  }
+
+  Widget _buildContent(List<Transaksi> data) {
+    return TabBarView(
+        children: [lastWeekTab(data), lastMonthTab(data), earlierTab(data)]);
   }
 
   Future<void> readJSON() async {
@@ -135,93 +164,97 @@ class _ActivityPageState extends State<ActivityPage> {
     });
   }
 
-  Widget todayTab() {
+  Future<List<Transaksi>?> fetchData() async {
+    Pelanggan? pel = await PelangganApi.getCurrentPelanggan(
+        Provider.of<AuthState>(context, listen: false).currentUser!.id);
+    print(pel!.id);
+    return TransaksiApi.getByPelanggan(pel.id);
+  }
+
+  Widget lastWeekTab(List<Transaksi> data) {
+    //filter last week
+    DateTime now = DateTime.now();
+    DateTime lastWeek = DateTime(now.year, now.month, now.day - 7);
+    List<Transaksi> lastWeekData = data.where((element) {
+      return element.createdAt.isAfter(lastWeek);
+    }).toList();
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Today",
-            textAlign: TextAlign.left,
-            style: GoogleFonts.poppins(
-                fontSize: 12,
-                color: AppColors.N800,
-                fontWeight: FontWeight.w600),
-          ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _items['today']?.length ?? 0,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return DetailActivityPage(
-                          activity: _items['today'][index]);
-                    }));
-                  },
-                  child: TransactionCard(
-                    vehicleName: _items['today'][index]["tipe"],
-                    transmissionType: _items['today'][index]["transmisi"],
-                    date: _items['today'][index]["date"],
-                    price: _items['today'][index]["price"],
-                    status: _items['today'][index]["status"],
-                    orderNumber: _items['today'][index]["orderNumber"],
-                    imagePath:
-                        'assets/images/${_items['today'][index]["tipe"]}.png',
-                  ),
-                );
-              },
-            ),
+            child: _buildTabContent(lastWeekData),
           ),
         ],
       ),
     );
   }
 
-  Widget weekTab() {
+  Widget lastMonthTab(List<Transaksi> data) {
+    //filter between last 30 day and last 7 day
+    DateTime now = DateTime.now();
+    DateTime lastMonth = DateTime(now.year, now.month, now.day - 30);
+    DateTime lastWeek = DateTime(now.year, now.month, now.day - 7);
+
+    List<Transaksi> lastMonthData = data.where((element) {
+      return element.createdAt.isAfter(lastMonth) &&
+          element.createdAt.isBefore(lastWeek);
+    }).toList();
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "This Week",
-            textAlign: TextAlign.left,
-            style: GoogleFonts.poppins(
-                fontSize: 12,
-                color: AppColors.N800,
-                fontWeight: FontWeight.w600),
-          ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _items['week']?.length ?? 0,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return DetailActivityPage(
-                          activity: _items['week'][index]);
-                    }));
-                  },
-                  child: TransactionCard(
-                    vehicleName: _items['week'][index]["tipe"],
-                    transmissionType: _items['week'][index]["transmisi"],
-                    date: _items['week'][index]["date"],
-                    price: _items['week'][index]["price"],
-                    status: _items['week'][index]["status"],
-                    orderNumber: _items['week'][index]["orderNumber"],
-                    imagePath:
-                        'assets/images/${_items['today'][index]["tipe"]}.png',
-                  ),
-                );
-              },
-            ),
+            child: _buildTabContent(lastMonthData),
           )
         ],
       ),
+    );
+  }
+
+  Widget earlierTab(List<Transaksi> data) {
+    //filter last week
+    DateTime now = DateTime.now();
+    DateTime lastMonth = DateTime(now.year, now.month, now.day - 30);
+    List<Transaksi> earlierData = data.where((element) {
+      return element.createdAt.isBefore(lastMonth);
+    }).toList();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Expanded(child: _buildTabContent(earlierData))],
+      ),
+    );
+  }
+
+  Widget _buildTabContent(List<Transaksi> data) {
+    if (data.isEmpty) {
+      return const Center(
+        child: Text("No Data"),
+      );
+    }
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailActivityPage(
+                transaksi: data[index],
+              );
+            }));
+          },
+          child: TransactionCard(
+            transaksi: data[index],
+          ),
+        );
+      },
     );
   }
 }
