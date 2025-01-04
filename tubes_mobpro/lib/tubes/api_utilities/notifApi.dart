f5158cb4db883f8c883264a10544f3d49b1b87de
haidar <haidarah134@gmail.com>
2025-01-04 16:53:39 +0700

    fix notifApi read method

--- tubes_mobpro/lib/tubes/api_utilities/notifApi.dart
+++ tubes_mobpro/lib/tubes/api_utilities/notifApi.dart
@@ -17,16 +17,12 @@ class NotifApi extends BaseApi {
       throw Exception("Exception: ${response.body}");
   }
 
-  static Future<List<Motor>> read(int notifId) async {
+  static Future<void> read(int notifId) async {
     var response = await BaseApi.getAuth("/api/notif/read/$notifId");
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body) as List;
-
-      return data.map((json) => Motor.fromJson(json)).toList();
+    if (!response.ok) {
+      throw Exception("Exception: ${response.body}");
     }
-
-    throw Exception("Exception: ${response.body}");
   }
 
   static Future<void> register(String deviceToken) async {
